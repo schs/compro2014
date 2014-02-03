85c6919759d5f25edae393606fda5a5e07824337
santiamchristian <lahmannm@santiam.org>
2014-02-03 13:09:49 -0800

    Update app.ts

--- compro2013/compro2013/app.ts
+++ compro2013/compro2013/app.ts
@@ -10,7 +10,7 @@ class Greeter {
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
-        this.test = "tester";
+        this.test = "test";
 
     }
 
