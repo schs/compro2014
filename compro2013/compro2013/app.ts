3914a54464ddae316dabab173a8db1cceb306e9d
J-Swerve <kurianja@students.santiam.org>
2014-02-04 10:20:11 -0800

    I changed test to testes

--- compro2013/compro2013/app.ts
+++ compro2013/compro2013/app.ts
@@ -10,8 +10,7 @@ class Greeter {
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
-        this.test = "test";
-        "You cant stop me"  
+        this.test = "testes";
 
     }
 
