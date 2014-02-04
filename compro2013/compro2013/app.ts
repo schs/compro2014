4ced1960c4e2b53201c8606dc91288af7fac7932
Penner <pennerlu@students.santiam.org>
2014-02-04 10:25:38 -0800

    no

--- compro2013/compro2013/app.ts
+++ compro2013/compro2013/app.ts
@@ -10,7 +10,7 @@ class Greeter {
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
-        this.test = "testes";
+        this.test = "yay";
 
     }
 
