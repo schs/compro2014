4bca5e941ed7736198acb4b5271ec61e4ce7034b
peterson <petersonse@students.santiam.org>
2014-02-05 13:45:51 -0800

    Fixed Item Issue

--- compro2013/compro2013/Source/Items/Item.ts
+++ compro2013/compro2013/Source/Items/Item.ts
@@ -1,1 +1,24 @@
- 
+class Item extends eg.Collision.Collidable {
+    name: String;
+    size: number;
+    scene: eg.Rendering.Scene2d;
+    lastCollision: eg.Collision.Collidable;
+    sprite: eg.Graphics.Sprite2d;
+
+    constructor(scene: eg.Rendering.Scene2d) {
+        this.scene = scene;
+        this.sprite
+        this.scene.Add(this.sprite);
+        super();
+    }
+
+
+    collision() {
+
+
+    }
+    Update(gameTime: eg.GameTime) {
+
+    }
+
+} 
