06c4d9bc5cd796f43cc03be8ca02a5ff46c4a7f0
peterson <petersonse@students.santiam.org>
2014-02-11 12:18:20 -0800

    Began adding properties and methods

--- compro2013/compro2013/Source/Items/Weapons/Sword.ts
+++ compro2013/compro2013/Source/Items/Weapons/Sword.ts
@@ -1,1 +1,30 @@
- 
+class Sword extends Item {
+    name: "Sword";
+    size: //number
+    scene:
+    lastCollision: eg.Collision.Collidable;
+    sprite: eg.Graphics.Sprite2d;
+
+    constructor() {
+
+
+
+        super();
+    }
+
+
+    collision() {
+
+    }
+
+    Update(gameTime: eg.GameTime) {
+    }
+
+
+
+
+
+
+
+
+}
