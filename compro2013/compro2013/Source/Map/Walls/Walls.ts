ec91e40e3e29f589cda1413234e0cef242c94678
Penner <pennerlu@students.santiam.org>
2014-02-12 13:24:56 -0800

    wal

--- compro2013/compro2013/Source/Map/Walls/Walls.ts
+++ compro2013/compro2013/Source/Map/Walls/Walls.ts
@@ -1,20 +1,21 @@
 class Wall extends eg.Collision.Collidable {
     shape: eg.Graphics.Shape;
+    scene: eg.Rendering.Scene2d;
+    collisionManager: eg.Collision.CollisionManager;
 
 
 
 
+    constructor(x: number, y: number, width: number, height: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager) {
+        this.scene = scene;
+        this.shape = new eg.Graphics.Rectangle(x, y, width, height);
+        this.collisionManager = collisionManager;
 
 
-    //constructor(x) {
-       // this.shape = new eg.Graphics.Rectangle
-
-
-
-//super();
-   // }
-
-
+        super(this.shape.GetDrawBounds());
+        this.scene.Add(this.shape);
+        this.collisionManager.Monitor(this);
+    }
 
 
 
