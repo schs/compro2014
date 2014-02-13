38148f75f55b955807b2f04ea6813b7fdd9f79fa
peterson <petersonse@students.santiam.org>
2014-02-13 11:47:41 -0800

    added damage to constructor

--- compro2013/compro2013/Source/Items/MeleeWeapon.ts
+++ compro2013/compro2013/Source/Items/MeleeWeapon.ts
@@ -2,8 +2,8 @@ class MeleeWeapon extends Item {
     name: string;
     damage: number;
 
-    constructor(name: string, scene: eg.Rendering.Scene2d, sprite: eg.Graphics.Sprite2d) {
-        //Set damage from parameter
+    constructor(name: string, scene: eg.Rendering.Scene2d, sprite: eg.Graphics.Sprite2d, damage: number) {
+        this.damage = damage;
         this.name = name;
         super("MeleeWeapon", scene, sprite);
     }
