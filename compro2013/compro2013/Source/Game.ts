65b6ea32ba99ef5af91f6ef8e22ac34d83f8c2d7
Burrow <burrowlu@students.santiam.org>
2014-02-18 12:16:29 -0800

    Edited Player and Game

--- compro2013/compro2013/Source/Game.ts
+++ compro2013/compro2013/Source/Game.ts
@@ -9,12 +9,13 @@ class Game extends eg.Game {
 
         this.mapHandler = new MapHandler(this.Scene, this.CollisionManager);
         this.mapHandler.load("/Source/Map/Maps/OverWorld.json", this.mapHandler.loadComplete);
-        
+        this.player = new Player(0,0 , ["Up"], ["Down"], ["Left"], ["Right"], this.Input.Keyboard, this.Scene);
 
     }
 
     Update(gameTime: eg.GameTime) {
-       
+        if (this.player.movementController)
+        this.Scene.Camera.Position = this.player.movementController.Position.Clone();
     }
 
 
