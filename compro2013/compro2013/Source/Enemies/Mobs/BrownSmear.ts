ab6fc225d372c2b0b8bcdc5786c37830626dbb19
CapitolLights <stevn_miller@q.com>
2014-02-18 12:15:29 -0800

    created BrownSmear class and added image

--- compro2013/compro2013/Source/Enemies/Mobs/BrownSmear.ts
+++ compro2013/compro2013/Source/Enemies/Mobs/BrownSmear.ts
@@ -1,1 +1,14 @@
- 
+class BrownSmear extends Enemies implements eg.IUpdateable {
+
+    
+
+    constructor(x: number, y: number, scene: eg.Rendering.Scene2d) {
+
+
+
+        super(100, 100, 100, 1, 1, new eg.Graphics.ImageSource("/images/BrownSmear.png", 100, 100), scene)
+
+    }
+
+
+} 
