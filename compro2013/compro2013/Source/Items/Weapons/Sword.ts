595a7379e87aa4ee3a7a3b326ae4e3b10bc40c64
Nguyen <nguyenth@students.santiam.org>
2014-02-19 13:25:36 -0800

    Changed the location of Sword.png

--- compro2013/compro2013/Source/Items/Weapons/Sword.ts
+++ compro2013/compro2013/Source/Items/Weapons/Sword.ts
@@ -2,7 +2,7 @@ class Sword extends MeleeWeapon {
     
     constructor(scene: eg.Rendering.Scene2d, damage: number) {
         this.damage= 50;
-        this.sprite= new eg.Graphics.Sprite2d(25,25, new eg.Graphics.ImageSource("/Images/Weapons/Sword Again2.png", 50 ,50)); 
+        this.sprite= new eg.Graphics.Sprite2d(25,25, new eg.Graphics.ImageSource("/Images/Items/Weapons/Sword.png", 50 ,50)); 
         super("Sword", scene, this.sprite, 10);
         this.scene.Add(this.sprite);
     }
