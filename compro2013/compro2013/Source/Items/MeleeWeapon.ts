3bf58b3b3357a59437404b1a6337b5f58cfe08d7
peterson <petersonse@students.santiam.org>
2014-02-24 11:58:59 -0800

    fixed sprite image source

--- compro2013/compro2013/Source/Items/MeleeWeapon.ts
+++ compro2013/compro2013/Source/Items/MeleeWeapon.ts
@@ -2,7 +2,7 @@ class MeleeWeapon extends Item {
     name: string;
     damage: number;
 
-    constructor(name: string, scene: eg.Rendering.Scene2d, sprite: eg.Graphics.Sprite2d, damage: number) {
+    constructor(name: string, scene: eg.Rendering.Scene2d, sprite: eg.Graphics.ImageSource, damage: number) {
         this.damage = damage;
         this.name = name;
         super("MeleeWeapon", scene, sprite);
