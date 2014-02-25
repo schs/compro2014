70fa326e61ec8334a12247378d2f9e95ac5edfd7
Nguyen <nguyenth@students.santiam.org>
2014-02-25 12:41:55 -0800

    Changed which sword was being used

--- compro2013/compro2013/Source/Items/Weapons/Sword.ts
+++ compro2013/compro2013/Source/Items/Weapons/Sword.ts
@@ -2,7 +2,7 @@ class Sword extends MeleeWeapon {
     
     constructor(x: number, y: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager) {
         
-        super(x,y,50,"Sword", scene, new eg.Graphics.ImageSource("/Resources/Images/Items/Weapons/Sword.png", 64, 64));
+        super(x,y,50,"Sword", scene, new eg.Graphics.ImageSource("/Resources/Images/Items/Weapons/Sword1.png", 64, 64));
 
     }
 }
