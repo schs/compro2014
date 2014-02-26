fdbb9680457fdc41e2a3f759c7281484da4fdf81
peterson <petersonse@students.santiam.org>
2014-02-26 13:15:35 -0800

    Created axe class

---
+++ compro2013/compro2013/Source/Items/Weapons/Axe.ts
@@ -0,0 +1,8 @@
+class Axe extends MeleeWeapon {
+
+    constructor(x: number, y: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager) {
+
+        super(x, y, 50, "Axe", scene, new eg.Graphics.ImageSource("/Resources/Images/Items/Weapons/Axe1.png", 64, 64));
+
+    }
+}
