511dcb3440d5a33a94e5edba4b7a90fca0daa3ff
all man <cool_ryman@comcast.net>
2014-03-11 12:19:11 -0700

    added attack to melee

--- compro2013/compro2013/Source/Attacks/Attack.ts
+++ compro2013/compro2013/Source/Attacks/Attack.ts
@@ -1,12 +1,17 @@
-class Attack extends eg.Collision.Collidable implements eg.IUpdateable {
+class Attack extends eg.Collision.Collidable implements ICollidableTyped {
     sprite: eg.Graphics.Sprite2d;
+    collisionType: CollisionType;
     speed: number;
     timer: number;
+    damage: number;
+    knockback: number;
     attacking: boolean;
 
         
 
-    constructor(position: eg.Vector2d, size: eg.Size2d) {
+    constructor(position: eg.Vector2d, size: eg.Size2d, damage: number, knockback: number) {
+        this.knockback = knockback;
+        this.damage = damage;
 
         super(new eg.Bounds.BoundingRectangle(position, size))
         
@@ -20,11 +25,17 @@ class Attack extends eg.Collision.Collidable implements eg.IUpdateable {
 
 
     }
-           
+
+    Collided(data: eg.Collision.CollisionData) {
+        var collider: ICollidableTyped = <ICollidableTyped>data.With;
 
 
-    Update (gameTime: eg.GameTime) {
-        
+
+    }
+
+
+    Update (gameTime: eg.GameTime, position: eg.Vector2d) {
+        this.Bounds.Position = position.Clone();
 
     }
 
--- compro2013/compro2013/Source/Items/MeleeWeapon.ts
+++ compro2013/compro2013/Source/Items/MeleeWeapon.ts
@@ -2,17 +2,23 @@ class MeleeWeapon extends Item {
     name: string;
     damage: number;
     attack: Attack;
+    knockback: number;
 
 
-    constructor(x: number, y: number, damage: number, name: string, scene: eg.Rendering.Scene2d, spriteImage: eg.Graphics.ImageSource) {
+    constructor(x: number, y: number, knockback: number, damage: number, name: string, scene: eg.Rendering.Scene2d, spriteImage: eg.Graphics.ImageSource) {
         this.damage = damage;
         this.name = name;
+        this.attack = new Attack(this.Bounds.Position, this.sprite.Size, this.damage, this.knockback);
         super(x, y, "MeleeWeapon", scene, spriteImage);
     }
 
     Attack() {
 
 
+    }
+
+    Update(gameTime: eg.GameTime, position: eg.Vector2d) {
+        this.attack.Update(gameTime, position);
     }
 
 
