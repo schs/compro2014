ba29be4369d54916949f01da6c687f1dac4141b8
Nguyen <sirwubbly@gmail.com>
2014-03-20 12:14:27 -0700

    Added RangedWeapon.ts and Crossbow.ts

---
+++ compro2013/compro2013/Source/Items/RangedWeapon.ts
@@ -0,0 +1,14 @@
+﻿class RangedWeapon extends Item {
+    name: string;
+    damage: number;
+    private attack: Attack;
+
+
+    constructor(x: number, y: number, damage: number, name: string, scene: eg.Rendering.Scene2d, spriteImage: eg.Graphics.ImageSource) {
+        this.damage = damage;
+        this.name = name;
+
+        super(x, y, "RangedWeapon", scene, spriteImage);
+    }
+
+}
