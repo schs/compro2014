156ffd3b5b2ba68211d763303c399c0e3e42a227
all man <cool_ryman@comcast.net>
2014-04-02 12:14:41 -0700

    nothing happened

--- compro2013/compro2013/Source/Player/Player.ts
+++ compro2013/compro2013/Source/Player/Player.ts
@@ -24,7 +24,6 @@ class Player extends eg.Collision.Collidable implements eg.IUpdateable, ICollida
 
     constructor(x: number, y: number, upKeys: string[], downKeys: string[], leftKeys: string[], rightKeys: string[], input: eg.Input.InputManager, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager) {
         this.inventory = [];
-        this.scene
         this.collisionType = CollisionType.Player;
         this.scene = scene;
         this.collisions = 0;
@@ -61,7 +60,7 @@ class Player extends eg.Collision.Collidable implements eg.IUpdateable, ICollida
 
     Attack() {
         if (this.leftHand)
-            this
+            this.Attack.e
 
 
     }
