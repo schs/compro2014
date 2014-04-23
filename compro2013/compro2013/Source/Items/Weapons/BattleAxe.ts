2fdbf393a467361119073ae58753db601e0738c7
Nguyen <sirwubbly@gmail.com>
2014-04-23 12:58:41 -0700

    Added arrows and a new weapon

---
+++ compro2013/compro2013/Source/Items/Weapons/BattleAxe.ts
@@ -0,0 +1,8 @@
+﻿class BattleAxe extends MeleeWeapon {
+
+    constructor(x: number, y: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager) {
+
+        super(x, y, 10, 30, "BattleAxe", scene, new eg.Graphics.ImageSource("/Resources/Images/Items/Weapons/BattleAxe.png", 64, 64), collisionManager);
+
+    }
+}
