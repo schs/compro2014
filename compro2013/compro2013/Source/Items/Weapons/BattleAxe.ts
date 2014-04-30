43d56b46b8f96e417e3d5ad27365b3a0f000cd4e
Nguyen <sirwubbly@gmail.com>
2014-04-30 13:13:14 -0700

    Changed some sprites and added the "plumbing"

--- compro2013/compro2013/Source/Items/Weapons/BattleAxe.ts
+++ compro2013/compro2013/Source/Items/Weapons/BattleAxe.ts
@@ -2,7 +2,7 @@
 
     constructor(x: number, y: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager) {
 
-        super(x, y, 10, 30, "BattleAxe", scene, new eg.Graphics.ImageSource("/Resources/Images/Items/Weapons/BattleAxe.png", 64, 64), collisionManager);
+        super(x, y, 10, 50, "BattleAxe", scene, new eg.Graphics.ImageSource("/Resources/Images/Items/Weapons/BattleAxe.png", 64, 64), collisionManager);
 
     }
 }
