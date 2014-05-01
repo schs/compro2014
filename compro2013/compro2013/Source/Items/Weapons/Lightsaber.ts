efb513ab4d49db9581b45843e1db2cc3e6a1c95d
Nguyen <sirwubbly@gmail.com>
2014-05-01 11:49:50 -0700

    Added an alternate, blue lightsaber sprite

--- compro2013/compro2013/Source/Items/Weapons/Lightsaber.ts
+++ compro2013/compro2013/Source/Items/Weapons/Lightsaber.ts
@@ -2,7 +2,7 @@
 
     constructor(x: number, y: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager) {
 
-        super(x, y, 100, 0, "Lightsaber", scene, new eg.Graphics.ImageSource("/Resources/Images/Items/Weapons/Lightsaber.png", 64, 64), collisionManager);
+        super(x, y, 100, 0, "Lightsaber", scene, new eg.Graphics.ImageSource("/Resources/Images/Items/Weapons/BlueLightsaber.png", 64, 64), collisionManager);
 
     }
 
