4c0047403348c7eafef9efe66fa7172b0f212982
samclarkhoward <kipcoindustries13@hotmail.com>
2014-05-07 12:32:14 -0700

    toast done

--- compro2013/compro2013/Source/Enemies/Mobs/Bosses/ToastMonster.ts
+++ compro2013/compro2013/Source/Enemies/Mobs/Bosses/ToastMonster.ts
@@ -2,7 +2,7 @@ class ToastMonster extends Enemy {
 
     constructor(x: number, y: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager, enemies: Enemy[],
         items: Item[]) {
-        super(100, 2, 6, 3, x, y, new eg.Graphics.ImageSource("/Resources/Images/Enemies/Bosses/ToastMonster.png", 1920, 128), 13, 18, 128, scene, collisionManager, enemies, items)
+        super(100, 2, 6, 3, x, y, new eg.Graphics.ImageSource("/Resources/Images/Enemies/Bosses/ToastMonster.png", 1920, 128), 15, 20, 128, scene, collisionManager, enemies, items)
 
     }
 } 
