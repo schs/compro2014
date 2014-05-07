75fb4af057406e656e3366b5b651f05bbd83eaf7
carbonaroet <esqueek22@gmail.com>
2014-05-07 11:52:46 -0700

    Added landicrab

---
+++ compro2013/compro2013/Source/Enemies/Mobs/LandiCrab.ts
@@ -0,0 +1,8 @@
+﻿class LandiCrab extends Enemy {
+
+    constructor(x: number, y: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager, enemies,
+        items: Item[]) {
+        super(100, 2, 6, 5, x, y, new eg.Graphics.ImageSource("/Resources/Images/Enemies/LandiCrab.png", 576, 64), 9, 10, 64, scene, collisionManager, enemies, items)
+
+    }
+} 
