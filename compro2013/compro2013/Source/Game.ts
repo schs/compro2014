befaf1bc4818c99767364cab4218e812bad857c3
all man <cool_ryman@comcast.net>
2014-05-14 12:09:33 -0700

    canvas better

--- compro2013/compro2013/Source/Game.ts
+++ compro2013/compro2013/Source/Game.ts
@@ -8,28 +8,37 @@ class Game extends eg.Game {
    
 
     constructor() {
-        super();
+        var canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("Content");
+        super(canvas);
         this.Scene.DrawArea.style.backgroundColor = "black";
         this.mapHandler = new MapHandler(this.Scene, this.CollisionManager, this.Input);
         this.mapHandler.load("/Source/Map/Maps/OverWorld.json");
-        
+        this.Scene.DrawArea.height = 720;
+        this.Scene.DrawArea.width = 1280;
+
 
        
     }
 
     Update(gameTime: eg.GameTime) {
         this.mapHandler.Update(gameTime);
-      
+        this.updateCanvasSize();
     }
 
     public updateCanvasSize() {
-        if (this.Scene.DrawArea.width !== window.innerWidth) {
-            var resizeAmmount = (window.innerWidth - this.Scene.DrawArea.width) / 2;
-            this.Scene.DrawArea.width = window.innerWidth;
-        }
-        if (this.Scene.DrawArea.height !== window.innerHeight) {
-            var resizeAmmount = (window.innerHeight - this.Scene.DrawArea.height) / 2;
-            this.Scene.DrawArea.height = window.innerHeight;
-        }
+        var canvas: JQuery = $("#Content");
+        canvas.width(window.innerWidth);
+        canvas.height(window.innerWidth / 1.777777777777778);
+
+        //if (this.Scene.DrawArea.width !== window.innerWidth) {
+        //    var resizeAmmount = (window.innerWidth - this.Scene.DrawArea.width) / 2;
+        //    this.Scene.DrawArea.width = window.innerWidth;
+        //}
+        //if (this.Scene.DrawArea.height !== window.innerHeight) {
+        //    var resizeAmmount = (window.innerHeight - this.Scene.DrawArea.height) / 2;
+        //    this.Scene.DrawArea.height = window.innerHeight;
+        //}
+
+        
     }
 } 
