d61c38415e909faf932cdb32f482660e722c4bbf
samclarkhoward <kipcoindustries13@hotmail.com>
2014-05-22 12:07:31 -0700

    chikin

---
+++ compro2013/compro2013/Source/Enemies/Mobs/Bosses/DooDooChicken.ts
@@ -0,0 +1,15 @@
+class DooDooChicken extends Enemy {
+    mapHandler: MapHandler;
+
+    constructor(x: number, y: number, scene: eg.Rendering.Scene2d, collisionManager: eg.Collision.CollisionManager, enemies: Enemy[], items: Item[], mapHandler: MapHandler) {
+        super(100, 2, 6, 3, x, y, new eg.Graphics.ImageSource("/Resources/Images/Enemies/Bosses/DooDooChicken.png", 1024, 128), 8, 20, 128, scene, collisionManager, enemies, items)
+        this.mapHandler = mapHandler;
+    }
+
+
+    ItemDrop() {
+        this.mapHandler.entrances.push(new BossEntrance(this.movementController.Position, "/Source/Map/Maps/OverWorld.json", this.mapHandler, this.collisionManager));
+
+    }
+
+} 
